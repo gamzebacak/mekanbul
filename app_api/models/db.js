bb9c612b81da768d1e3c6fb9b5fc48b3284420ad
gamzebacak <gamzebck2513@gmail.com>
2022-11-29 14:19:42 +0300

    gonderim

--- app_api/models/db.js
+++ app_api/models/db.js
@@ -1,27 +1,31 @@
-var mongoose=require("mongoose");
+var mongoose = require("mongoose");
 require("./mekansema");
+//var dbURI = 'mongodb+srv://pgamic:pgamic.2513.@mekanbul.r7bckex.mongodb.net/?retryWrites=true&w=majority';
+var dbURI = 'mongodb://localhost/mekanbul';
 
-var dbURI="mongodb://localhost/mekanbul";
-//var dbURI="mongodb+srv://pgamic:pgamic.2513.@mekanbul.r5jza4v.mongodb.net/mekanbul?retryWrites=true&w=majority";
 mongoose.connect(dbURI);
-function kapat(msg,callback){
-    mongoose.connection.close(function(){
+function kapat(msg, callback) {
+    mongoose.connection.close(function () {
         console.log(msg);
-        callback(); 
+        callback();
     });
 }
-process.on("SIGINT",function(){
-    kapat("Uygulama kapatıldı!",function(){
+process.on("SIGINT", function () {
+    kapat("Uygulama kapatıldı!", function () {
         process.exit(0);
-    });
-});
-mongoose.connection.on("connected",function(){
-    console.log(dbURI+" adresindeki veritabanına bağlandı");
-});
-mongoose.connection.on("disconnected",function(){
-    console.log(dbURI+" adresindeki veritabanı bağlantısı koptu");
-});
-mongoose.connection.on("error",function(){
+    }
+    );
+}
+);
+mongoose.connection.on("connected", function () {
+    console.log(dbURI + " adresindeki veritabanına bağlanıldı")
+}
+);
+mongoose.connection.on("disconnected", function () {
+    console.log(dbURI + " adresindeki veritabanı bağlantısı koptu");
+}
+);
+mongoose.connection.on("error", function () {
     console.log("Bağlantı hatası");
-});
-
+}
+);
