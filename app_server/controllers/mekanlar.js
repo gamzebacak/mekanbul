efd0e391e11e2df55c2b9b9b577bfc6f0364dffc
gamzebacak <gamzebck2513@gmail.com>
2022-12-05 14:43:08 +0300

    Son

--- app_server/controllers/mekanlar.js
+++ app_server/controllers/mekanlar.js
@@ -1,86 +1,104 @@
-var express = require('express');
-var router = express.Router();
+const axios = require("axios")
 
-
-const anaSayfa = function (req, res, next) {
-    res.render('anasayfa', 
-    { "baslik" : "Ana Sayfa",
-    "sayfaBaslik":{
-        "siteAd":"Mekanbul",
-        "slogan":"Civardaki mekanları keşfet!"
-    },
-    "mekanlar":[
-        {
-            "ad":"Barida Kafe",
-            "adres":"SDÜ Batı Kampüsü",
-            "puan":"4",
-            "mesafe":"2km",
-            "imkanlar":["Kahve","Çay","Pasta"]
-        },
-        {
-            "ad":"Gloria",
-            "adres":"SDÜ Doğu Kampüsü",
-            "puan":"3",
-            "mesafe":"5km",
-            "imkanlar":["Kahve","Çay","Pasta"]
-        }
-    ]
-
-    });
+var apiSecenekleri = {
+  sunucu: "https://mekanbul-1.loiyhabeeb.repl.co",
+  apiYolu: "/api/mekanlar/"
 }
-const mekanBilgisi = function (req, res, next) {
-    res.render('mekanbilgisi',
-     { title: 'Mekan Bilgisi',
-    "mekanBaslik":"Starbucks",
-    "mekanDetay":{
-        "ad":"Starbucks",
-        "adres":"Centrum Garden",
-        "puan":"4",
-        "saatler":[
-            {
-                "gunler":"Pazartesi-Cuma",
-                "acilis":"9:00",
-                "kapanis":"23:00",
-                "kapali":false
-            },
-            {
-                "gunler":"Cumartesi-Pazar",
-                "acilis":"10:00",
-                "kapanis":"24:00",
-                "kapali":false
-            }
-        ],
-        "imkanlar":["Kahve","Çay","Kek"],
-        "koordinatlar":{
-            "enlem":"37.7",
-            "boylam":"30.5"
-        },
-        "yorumlar":[
-            {
-                "yorumYapan":"Sinan",
-                "puan":"4",
-                "tarih":"20 Ekim 2022",
-                "yorumMetni":"Çok beğendim."
-            },
-            {
-                "yorumYapan":"Gamze",
-                "puan":"1",
-                "tarih":"20 Ekim 2022",
-                "yorumMetni":"Bidaha asla gelmem."
-            }
-        ]
-        
+
+var mesafeyiFormatla = function (mesafe) {
+  var yeniMesafe, birim
+  if (mesafe > 1) {
+    yeniMesafe = parseFloat(mesafe).toFixed(1)
+    birim = " km"
+  }
+  else {
+    yeniMesafe = parseInt(mesafe * 1000, 10)
+    birim = " m"
+  }
+  return yeniMesafe + birim
+}
+
+const anaSayfaOlustur = function (res, mekanListesi) {
+  var mesaj
+  if (!(mekanListesi instanceof Array)) {
+    mesaj = "API hatası!"
+    mekanListesi = []
+  }
+  else {
+    if (!mekanListesi.length) {
+      mesaj = "Civarda herhangi bir mekan bulunamadı."
     }
-});
-}
-const yorumEkle = function (req, res, next) {
-    res.render('yorumekle', { title: 'Yorum Ekle' });
+  }
+  res.render("anasayfa", {
+    "baslik": "Anasayfa",
+    "sayfaBaslik": {
+      "siteAd": "Mekanbul",
+      "slogan": "Mekanları Keşfet"
+    },
+    "mekanlar": mekanListesi,
+    "mesaj": mesaj
+  })
 }
 
+const anaSayfa = function (req, res) {
+  axios.get(apiSecenekleri.sunucu + apiSecenekleri.apiYolu, {
+    params: {
+      enlem: req.query.enlem,
+      boylam: req.query.boylam
+    }
+  }).then(function (response) {
+    var i, mekanlar
+    mekanlar = response.data
+    for (i = 0; i < mekanlar.length; i++) {
+      mekanlar[i].mesafe = mesafeyiFormatla(mekanlar[i].mesafe)
+    }
+    anaSayfaOlustur(res, mekanlar)
+  }).catch(function (hata) {
+    anaSayfaOlustur(res, hata)
+  })
+}
+
+const detaySayfasiOlustur = function (res, mekanDetaylari) {
+  mekanDetaylari.koordinat = {
+    "enlem": mekanDetaylari.koordinat[0],
+    "boylam": mekanDetaylari.koordinat[1]
+  }
+  res.render('mekanbilgisi', {
+    mekanBaslik: mekanDetaylari.ad,
+    mekanDetay: mekanDetaylari
+  })
+}
+
+const hataGoster = function (res, hata) {
+  var mesaj
+  if (hata.response.status == 404) {
+    mesaj = "404, Sayfa Bulunamadı!"
+  }
+  else {
+    mesaj = hata.response.status + " hatası"
+  }
+  res.status(hata.response.status)
+  res.render('error', {
+    "mesaj": mesaj
+  })
+}
+
+const mekanBilgisi = function (req, res, next) {
+  axios.get(apiSecenekleri.sunucu + apiSecenekleri.apiYolu + req.params.mekanid)
+    .then(function (response) {
+      detaySayfasiOlustur(res, response.data)
+    })
+    .catch(function (hata) {
+      hataGoster(res, hata)
+    })
+}
+
+const yorumEkle = function (req, res, next) {
+  res.render('yorumekle', { title: 'Yorum Ekle' });
+}
 
 module.exports = {
-    anaSayfa,
-    mekanBilgisi,
-    yorumEkle
-
+  anaSayfa,
+  mekanBilgisi,
+  yorumEkle
 }
